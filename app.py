813641b021d51bb849919495c5c9f493e8358a00
Eli <eziaiefar@gmail.com>
2017-05-04 12:01:40 -0500

    Add device class & forms

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SECRET_KEY'] = 'Iamtheverymodelofamodernmajorgeneral'
 
 __dbfn__ = "DVTCinventory"
 __sqlext__ = '.sqlite'
@@ -28,17 +28,30 @@ login_manager.login_view = 'login'
 class User(UserMixin, db.Model):
     __tablename__ = "people"
     id = db.Column(db.Integer, primary_key=True)
-    badge_id = db.Column(db.Integer, unique=True)
+    badge = db.Column(db.String(80), unique=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
-db.create_all()
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+class Phone(db.Model):
+    """  will add relations to User http://flask-sqlalchemy.pocoo.org/2.1/quickstart/"""
+    __tablename__ = "devices"
+    id = db.Column(db.Integer, primary_key=True)
+    MEID = db.Column(db.String(28))
+    OEM = db.Column(db.String(50))
+    SKU = db.Column(db.String(50))
+    IMEI = db.Column(db.String(50))
+    MODEL = db.Column(db.String(50))
+    Hardware_Type = db.Column(db.String(50))
+    In_Date = db.Column(db.String(50))
+    Out_Date = db.Column(db.String(50))
+    Archived = db.Column(db.String(50))
+    TesterName = db.Column(db.String(80))
+    DVT_Admin = db.Column(db.String(80))
+    Serial_Number = db.Column(db.String(50))
+    MSLPC = db.Column(db.String(50))
+    Comment = db.Column(db.String(255))
 
 
 class LoginForm(FlaskForm):
@@ -50,9 +63,34 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     __tablename__ = "devices"
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    badge = StringField('badge', validators=[InputRequired(), Length(min=4, max=80)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     admin = BooleanField('admin')
+
+
+class NewDevice(FlaskForm):
+    MEID = StringField('MEID', validators=[InputRequired(), Length(min=4, max=80)])
+    OEM =  StringField('OEM', validators=[InputRequired(), Length(min=4, max=80)])
+    SKU =  StringField('SKU', validators=[InputRequired(), Length(min=4, max=80)])
+    IMEI = StringField('IMEI', validators=[InputRequired(), Length(min=4, max=80)])
+    MODEL =  StringField('MODEL', validators=[InputRequired(), Length(min=4, max=80)])
+    Hardware_Type =  StringField('Hardware_Type', validators=[InputRequired(), Length(min=4, max=80)])
+    In_Date =  StringField('In_Date', validators=[InputRequired(), Length(min=4, max=80)])
+    Out_Date =  StringField('Out_Date', validators=[InputRequired(), Length(min=4, max=80)])
+    Archived =  StringField('Archived', validators=[InputRequired(), Length(min=4, max=80)])
+    TesterName =  StringField('TesterName', validators=[InputRequired(), Length(min=4, max=80)])
+    DVT_Admin =  StringField('DVT_Admin', validators=[InputRequired(), Length(min=4, max=80)])
+    Serial_Number =  StringField('Serial_Number', validators=[InputRequired(), Length(min=4, max=80)])
+    MSLPC =  StringField('MSLPC', validators=[InputRequired(), Length(min=4, max=80)])
+    Comment =  StringField('Comment', validators=[InputRequired(), Length(min=4, max=80)])
+
+db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
@@ -71,7 +109,7 @@ def login():
                 login_user(user, remember=form.remember.data)
                 return redirect(url_for('dashboard'))
 
-        return '<h1>Invalid username or password</h1>'
+        return redirect(url_for('signup'))
         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template('login.html', form=form)
@@ -80,10 +118,13 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = User(username=form.username.data,
+                        badge=form.badge.data,
+                        email=form.email.data,
+                        password=hashed_password
+                        )
         db.session.add(new_user)
         db.session.commit()
 
@@ -91,6 +132,33 @@ def signup():
         #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
+
+
+@app.route('/newdevice', methods=['GET', 'POST'])
+def newdevice():
+    form = NewDevice()
+    if form.validate_on_submit():
+        new_device = NewDevice(MEID = form.MEID.data,
+                               OEM = form.OEM.data,
+                               SKU = form.SKU.data,
+                               IMEI = form.IMEI.data,
+                               MODEL = form.MODEL.data,
+                               Hardware_Type = form.Hardware_Type.data,
+                               In_Date = form.In_Date.data,
+                               Out_Date = form.Out_Date.data,
+                               Archived = form.Archived.data,
+                               TesterName = form.TesterName.data,
+                               DVT_Admin = form.DVT_Admin.data,
+                               Serial_Number = form.SKU.data,
+                               MSLPC = form.SKU.data,
+                               Comment = form.SKU.data)
+        db.session.add(new_device)
+        db.session.commit()
+
+        return '<h1>New Device Entered!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('newdevice.html', form=form)
 
 
 @app.route('/dashboard')
